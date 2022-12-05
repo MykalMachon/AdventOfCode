b0c3ccd157fa55fc137802c612d7f885c5cd1d07
Mykal Machon <mykal@tinybox.dev>
2022-12-05 18:14:14 +0000

    feat: adding 2022/04 p2

--- 2022/04/__init__.py
+++ 2022/04/__init__.py
@@ -6,6 +6,13 @@ steps for part 1 of today's challenge:
 2. look at each pair and create ranges based on assignments
 3. use sets to find if either set is a subset of the other 
 4.1 if either of the sets are subsets, return True, else False.
+5. return the sum of sets that are subsets of their pair.
+
+steps for part 2 of today's challenge:
+1. use steps 1-2 of the above
+2. use sets to find if the two elements are disjoint.
+2.1 if the sets are disjoint, there is no overlap, there is if they are
+3. return the sum of sets with any intersect across pairs.
 """
 
 from typing import List, Tuple
@@ -37,19 +44,37 @@ def find_subsets(pairs: List[List[int]]):
     """
     subsets = []
     for pair in pairs:
-        if pair[0].issubset(pair[1]):
-            subsets.append(True)
-        elif pair[1].issubset(pair[0]):
+        if pair[0].issubset(pair[1]) or pair[1].issubset(pair[0]):
             subsets.append(True)
         else:
-          subsets.append(False)
+            subsets.append(False)
     return subsets
+
+
+def find_overlap(pairs: List[List[int]]):
+    """find pairs where there is any overlap at all
+    """
+    overlaps = []
+    for pair in pairs:
+        if pair[0].isdisjoint(pair[1]):
+            overlaps.append(False)
+        else:
+            overlaps.append(True)
+    return overlaps
 
 
 if __name__ == "__main__":
     print("starting AOC 2022 Day 04")
     input = load_input()
+
+    # part 1: find subset overlaps
     pairs = get_range_pairs(input)
     subset_overlap = find_subsets(pairs)
-    subset_overlap_sum = reduce(lambda a, c: a+1 if c is True else a, subset_overlap, 0)
-    print(f"there are {subset_overlap_sum} overlapping schedules")
+    subset_overlap_sum = reduce(
+        lambda a, c: a+1 if c is True else a, subset_overlap, 0)
+    print(f"there are {subset_overlap_sum} completely overlapping schedules")
+
+    # part 2: find any overlaps at all
+    overlap = find_overlap(pairs)
+    overlap_sum = reduce(lambda a, c: a+1 if c is True else a, overlap, 0)
+    print(f"there are {overlap_sum} partially overlapping schedules")
